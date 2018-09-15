9d5dbe0950afba87f7c698d4af90b02b8416496a
Boredmp <gizmos46@verizon.net>
2018-09-15 17:07:16 -0400

    updated data storage so get and post work

--- dataStorage.js
+++ dataStorage.js
@@ -2,12 +2,25 @@ var events = [];
 
 module.exports.handlePost = function(request)
 {
-  events.push(request.body);
-  console.log(request.body);
+  //events.push(request.body);
+  var data = JSON.parse(request.body);
+  fs.writeFile('data.json', data, (err) => {  
+    if (err) throw err;
+    console.log('Data written to file');
+  });
   return 'OK';
 };
 
-module.exports.handleGet = function()
+module.exports.handleGet = function(fs, requestedUser)
 {
-  return events;
+  var content = fs.readFileSync("data.json");
+  var jsonContent = JSON.parse(content);
+  var foundUser = {};
+
+  for (i in jsonContent){
+    if(jsonContent[i].username.includes(requestedUser)){
+        foundUser=jsonContent[i]
+    }
+  }
+  return foundUser;
 }
--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var express = require('express');
-var dataStorage = require('dataStorage');
+var dataStorage = require('./dataStorage.js');
+var bodyParser = require('body-parser');
 var app = express();
+var fs = require('fs');
 
-app.use(express.json());
 
-app.get('/', function(req, res)
+app.use(bodyParser.json())
+
+app.get('/api/getAccount/:user', function(req, res)
 {
-  res.send(dataStorage.handleGet());
+  const requestedUser = req.params['user'];
+  res.send(dataStorage.handleGet(fs, requestedUser));
 });
 
-app.post('/', function(req, res)
+app.post('/api/createAccount', function(req, res)
 {
-  res.send(dataStorage.handlePost(req));
+  res.send(201, dataStorage.handlePost(req));
 });
 
 app.listen(3000, function()
