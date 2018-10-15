7ea9aa03be23537afaef8fe11b03ddb1072c5ff6
Justin Dilks <justin.dilks@gmail.com>
2018-10-15 19:27:20 -0400

    adds (local only) Mongo integration for account

---
+++ dbDataStorage.js
@@ -0,0 +1,154 @@
+var DataValidator = require ('./dataValidator.js');
+
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/test");
+
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback) {
+    console.log("Connection succeeded.");
+});
+
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+	     username: String,
+	     password: String
+	 });
+
+module.exports.handlePost = function(request)
+{
+	 var User = mongoose.model("user", userSchema);
+	 
+	 var thisUser = new User({
+	     username: request.body.username,
+	     password: request.body.password
+	 });
+	 
+	 thisUser.save(function(error) {
+	     console.log("User has been saved!");
+	 if (error) {
+	     console.error(error);
+	  }
+	 });
+};
+
+var validateUserBody = function(body)
+{
+	if(body.username == null || typeof body.username !== string
+		|| body.password == null || typeof body.password !== string)
+		{
+			throw error;
+		}
+};
+
+module.exports.handleEventPost = function(request)
+{
+  var jsonContent = readDataStore('events.json');
+	console.log()
+  jsonContent.push(request.body);
+  fs.writeFile('events.json', JSON.stringify(jsonContent), (err) => {
+    if (err) throw err;
+    console.log('Data written to file');
+  });
+  return 'OK';
+};
+
+module.exports.handleReviewPost = function(request)
+{
+  var jsonContent = readDataStore('reviews.json');
+	console.log()
+  jsonContent.push(request.body);
+  fs.writeFile('reviews.json', JSON.stringify(jsonContent), (err) => {
+    if (err) throw err;
+    console.log('Data written to file ' + jsonContent);
+  });
+  return JSON.stringify(jsonContent);
+};
+
+module.exports.handleVendorPost = function(request)
+{
+	console.log(request.body);
+	var jsonContent = readDataStore('vendor.json');
+	jsonContent.push(request.body);
+	fs.writeFile('vendor.json', JSON.stringify(jsonContent), (err) => {
+    if (err) throw err;
+    console.log('Data written to file');
+  });
+  return 'OK';
+};
+
+module.exports.handleGetReviews = function(fs, vendorUsername)
+{
+  var jsonContent = readDataStore('reviews.json');
+  var foundUser = [];
+  for (entry in jsonContent) {
+    if(jsonContent[entry].vendorUsername.includes(vendorUsername)) {
+      foundUser.push(jsonContent[entry]);
+    }
+  }
+
+  return foundUser;
+}
+
+module.exports.handleGet = function(fs, requestedUser)
+{
+  	var User = mongoose.model("user", userSchema);
+
+	return User.find({username: requestedUser});
+};
+
+module.exports.handleGetEvents = function(fs)
+{
+  var jsonContent = readDataStore('events.json');
+  for (entry in jsonContent) {
+    var jsonVendorContent = readDataStore('vendor.json');
+    for (vendor in jsonVendorContent) {
+      if(jsonVendorContent[vendor].username == jsonContent[entry].vendorUsername) {
+        jsonContent[entry].vendor = jsonVendorContent[vendor];
+        break;
+      }
+    }
+  }
+  return jsonContent;
+}
+
+module.exports.handleGetEvent = function(fs, id)
+{
+  var jsonContent = readDataStore('events.json');
+  var foundEvent = {};
+  for (event in jsonContent) {
+    if(jsonContent[event].id == id) {
+      foundEvent = jsonContent[event];
+      var jsonVendorContent = readDataStore('vendor.json');
+      for (vendor in jsonVendorContent) {
+        if(jsonVendorContent[vendor].username == foundEvent.vendorUsername) {
+          foundEvent.vendor = jsonVendorContent[vendor];
+          break;
+        }
+      }
+      break;
+    }
+  }
+
+  return foundEvent;
+}
+
+module.exports.handleGetVendor = function(fs, vendorId)
+{
+  var jsonContent = readDataStore('vendor.json');
+  var foundUser = {};
+  for (entry in jsonContent) {
+    if(jsonContent[entry].id == vendorId) {
+      foundUser = jsonContent[entry];
+    }
+  }
+
+  return foundUser;
+}
+
+var readDataStore = function(fileName)
+{
+  var content = fs.readFileSync(fileName);
+  return JSON.parse(content);
+};
