b7ba347f04120db5b48470170baf2aab8d35ec34
Justin Dilks <justin.dilks@gmail.com>
2018-10-17 20:01:30 -0400

    more Mongo, updates account for menu

--- dbDataStorage.js
+++ dbDataStorage.js
@@ -12,9 +12,31 @@ db.once("open", function(callback) {
 
 var Schema = mongoose.Schema;
 var userSchema = new Schema({
-	     username: String,
-	     password: String
-	 });
+	username: String,
+	password: String,
+	menu:
+	[
+		{
+			name: String,
+			price: Number
+		}
+	]
+});
+
+var reviewSchema = new Schema({
+	vendorUsername:String,
+	comment: String,
+	rating: Number
+});
+
+var eventSchema = new Schema({
+	id:Number,
+	vendorUsername:String,
+	saleDescription:String,
+	start:String,
+	end:String,
+	address:String
+});
 
 module.exports.handleCreateAccountPost = function(request)
 {
@@ -33,37 +55,43 @@ module.exports.handleCreateAccountPost = function(request)
 	 });
 };
 
-var validateUserBody = function(body)
-{
-	if(body.username == null || typeof body.username !== string
-		|| body.password == null || typeof body.password !== string)
-		{
-			throw error;
-		}
-};
-
 module.exports.handleEventPost = function(request)
 {
-  var jsonContent = readDataStore('events.json');
-	console.log()
-  jsonContent.push(request.body);
-  fs.writeFile('events.json', JSON.stringify(jsonContent), (err) => {
-    if (err) throw err;
-    console.log('Data written to file');
-  });
-  return 'OK';
+  	var Event = mongoose.model("event", eventSchema);
+	 
+	 var thisEvent = new Event({
+	        id:request.body.id,
+		vendorUsername:request.body.vendorUsername,
+		saleDescription:request.body.saleDescription,
+		start:request.body.start,
+		end:request.body.end,
+		address:request.body.address
+	 });
+	 
+	 thisEvent.save(function(error) {
+	     console.log("Event has been saved!");
+	 if (error) {
+	     console.error(error);
+	  }
+	 });
 };
 
 module.exports.handleReviewPost = function(request)
 {
-  var jsonContent = readDataStore('reviews.json');
-	console.log()
-  jsonContent.push(request.body);
-  fs.writeFile('reviews.json', JSON.stringify(jsonContent), (err) => {
-    if (err) throw err;
-    console.log('Data written to file ' + jsonContent);
-  });
-  return JSON.stringify(jsonContent);
+	var Review = mongoose.model("review", reviewSchema);
+		 
+	 var thisReview = new Review({
+		vendorUsername: request.body.vendorUsername,
+		comment: request.body.comment,
+		rating: request.body.rating
+	 });
+	 
+	 thisReview.save(function(error) {
+	     console.log("Review has been saved!");
+	 if (error) {
+	     console.error(error);
+	  }
+	 });
 };
 
 module.exports.handleVendorPost = function(request)
@@ -78,17 +106,11 @@ module.exports.handleVendorPost = function(request)
   return 'OK';
 };
 
-module.exports.handleGetReviews = function(fs, vendorUsername)
+module.exports.handleGetReviews = function(fs, requestedUsername)
 {
-  var jsonContent = readDataStore('reviews.json');
-  var foundUser = [];
-  for (entry in jsonContent) {
-    if(jsonContent[entry].vendorUsername.includes(vendorUsername)) {
-      foundUser.push(jsonContent[entry]);
-    }
-  }
+ 	var Review = mongoose.model("review", reviewSchema);
 
-  return foundUser;
+	return Review.find({vendorUsername: requestedUsername});
 }
 
 module.exports.handleGet = function(fs, requestedUser)
--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.get('/api/getAccount/:user', function(req, res)
   var query =  dataStorage.handleGet(fs, requestedUser)
   query.exec(function(err,results){
    if(err)
-      res.send(err);
+      res.send(400, err);
    results.forEach(function(result){
-      res.send(result);
+      res.send(200, result);
    });
   });
 });
@@ -37,8 +37,13 @@ app.get('/api/getVendor/:id', function(req, res)
 
 app.get('/api/getReviews/:vendorUsername', function(req, res)
 {
-  const vendorUsername = req.params['vendorUsername'];
-  res.send(dataStorage.handleGetReviews(fs, vendorUsername));
+   const requestedUsername = req.params['vendorUsername'];
+  var query =  dataStorage.handleGetReviews(fs, requestedUsername)
+  query.exec(function(err,results){
+   if(err)
+      res.send(400, err);
+   res.send(200, results);
+  });
 });
 
 app.post('/api/createAccount', function(req, res)
@@ -57,7 +62,16 @@ app.post('/api/createAccount', function(req, res)
 
 app.post('/api/createReview', function(req, res)
 {
+	try
+	{
+		console.log("in create");
 		res.send(201, dataStorage.handleReviewPost(req));
+	}
+	catch (err)
+	{
+		console.log(err);
+		res.send(400, "invalid request");
+	}
 });
 
 app.post('/api/createEvent', function(req, res)
