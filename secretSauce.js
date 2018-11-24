49b908df450f086c302ba1d9293401e60c25a462
Justin Dilks <justin.dilks@gmail.com>
2018-11-24 06:53:41 -0500

    case insensitivity

--- secretSauce.js
+++ secretSauce.js
@@ -4,6 +4,6 @@ var reviewRating = 62.376;
 module.exports.applyRatings = function(event, userCity)
 {
 	var reviewScore = (event.averageScore - 2.5) * reviewRating;
-	var locationScore = null === userCity ? 0 : userCity === event.address ? locationRating : 0;
+	var locationScore = null === userCity || null === event.address ? 0 : event.address.toUpperCase().indexOf(userCity.toUpperCase()) !== -1 ? locationRating : 0;
 	event.relevancyScore = reviewScore + locationScore;
 };
